5be1153086475da86c1412c8f45142c5f74465b7
msy1332 <17322714918@163.com>
2025-05-29 13:12:00 +0800

    I've almost finished writing the 2D matrix. Come on!

--- 3D_Engine/DrawCubeMain.cpp
+++ 3D_Engine/DrawCubeMain.cpp
@@ -1,8 +1,29 @@
+#include "../Tools/_Matrix.h"
+#include "../Tools/_Vector.h"
 #include <iostream>		// 包含标准的输入输出头文件
 #include <easyx.h>		// 包含Easyx图形库
 using namespace std;    // 包含std命名空间
+using namespace _Matrix;    // 包含_Matrix命名空间,也就是包含矩阵的命名空间
+using namespace _Vector;    // 包含_Vector命名空间,也就是包含向量的命名空间
 
 int main() // 程序主函数，也就是程序的入口函数
 {
+	Vector2D v1(2, 3, 1); // 定义了一个二维的向量对象
+	Matrix2D v2(
+		Vector2D(1, 0, 2),
+		Vector2D(0, 1, 2),
+		Vector2D(0, 0, 1)
+	); // 定义了一个2D矩阵对象
+	Matrix2D v3(
+		Vector2D(2, 0, 0),
+		Vector2D(0, 2, 0),
+		Vector2D(0, 0, 1)
+	); // 定义了一个2D矩阵对象
+	Matrix2D v5 = v2 * v3; // 矩阵和矩阵相乘，并拷贝给新的2D矩阵对象
+	Vector2D v4 = v5 * v1; // 矩阵与向量相乘，并拷贝给新的2D向量对象
+	for (int i = 0; i < 3; i++)
+	{
+		cout << v4[i] << endl;
+	}
 	return 0; //程序的返回值，如果返回0代表程序正常退出，否则程序就是非正常退出
 }
--- Tools/_Matrix.cpp
+++ Tools/_Matrix.cpp
@@ -9,7 +9,7 @@ _Matrix::Matrix2D::Matrix2D()
 	_Matrix2D[2] = { 0,0,0 };
 }
 
-_Matrix::Matrix2D::Matrix2D(_Vector::Vector3D v1, _Vector::Vector3D v2, _Vector::Vector3D v3)
+_Matrix::Matrix2D::Matrix2D(_Vector::Vector2D v1, _Vector::Vector2D v2, _Vector::Vector2D v3)
 {
 	_Matrix2D[0] = v1;
 	_Matrix2D[1] = v2;
@@ -23,6 +23,18 @@ _Vector::Vector2D& _Matrix::Matrix2D::operator[](int index)
 		return _Matrix2D[index];
 	}
 	else 
+	{
+		throw std::out_of_range("Index out of range");
+	}
+}
+
+const _Vector::Vector2D& _Matrix::Matrix2D::operator[](int index) const
+{
+	if (index >= 0 && index <= 2)
+	{
+		return _Matrix2D[index];
+	}
+	else
 	{
 		throw std::out_of_range("Index out of range");
 	}
@@ -42,4 +54,54 @@ _Matrix::Matrix2D _Matrix::Matrix2D::operator*(const Matrix2D& v) const
 		}
 	}
 	return v1;
+}
+
+_Vector::Vector2D _Matrix::Matrix2D::operator*(const _Vector::Vector2D& v) const
+{
+	_Vector::Vector2D v1;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			v1[i] += _Matrix2D[i][j] * v[j];
+		}
+	}
+	return v1;
+}
+
+_Matrix::Matrix2D _Matrix::Matrix2D::operator+(const Matrix2D& v) const
+{
+	Matrix2D v1;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			v1[i][j] = _Matrix2D[i][j] + v[i][j];
+		}
+	}
+	return v1;
+}
+
+_Matrix::Matrix2D _Matrix::Matrix2D::operator-(const Matrix2D& v) const
+{
+	Matrix2D v1;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			v1[i][j] = _Matrix2D[i][j] - v[i][j];
+		}
+	}
+	return v1;
+}
+
+void _Matrix::Matrix2D::operator=(const Matrix2D& v)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			_Matrix2D[i][j] = v[i][j];
+		}
+	}
 }
