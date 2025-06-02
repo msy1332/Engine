4ad6c9b0b2b81ff99ed5954c3b829418a2a3b404
msy1332 <17322714918@163.com>
2025-06-02 20:43:12 +0800

    I reviewed it a bit, but it's still a bit boring. Come on! I'm sure I can understand it thoroughly.

--- 3D_Engine/DrawCubeMain.cpp
+++ 3D_Engine/DrawCubeMain.cpp
@@ -10,47 +10,53 @@ using namespace _Vector::Vector_2D;    // 包含Vector_2D命名空间,也就是包含向量的
 using namespace _Model::Model2D;	   // 包含Model命名空间，也就是包含这个2D模型的命名空间
 using namespace std;    // 包含std命名空间
 
-ExMessage msg; // 定义了一个消息结构体
-bool Running = true; // 程序的运行状态
+bool Running = true; // 程序的游戏状态
+ExMessage msg; // 定义了一个消息结构体，用来获取消息
 
-double r;  // 模型的绕世界坐标系旋转角度
-double sx; // 这个模型的向x缩放多少倍
-double sy; // 这个模型的向y缩放多少倍
-double tx; // 这个模型的向x移动多少个距离
-double ty; // 这个模型的向y移动多少个距离
+// 这个是这个三角形模型在世界坐标系中的位置和这个模型的旋转角度
+double X;
+double Y;
+double R;
 
-double X; // 模型在世界坐标系中的x坐标，也就是这个模型下的模型坐标系中的原点在世界坐标系中的位置
-double Y; // 模型在世界坐标系中的y坐标，也就是这个模型下的模型坐标系中的原点在世界坐标系中的位置
-double R; // 模型的旋转角度
-Model2D Triangle; // 定义了一个三角形模型
+// 定义一些矩阵用来给这个三角形模型进行矩阵的变换的，也就是说这些矩阵是用来给这个三角形模型进行旋转，平移，缩放变换的
+Matrix2D_Scaling Scaling;		// 定义了一个缩放矩阵
+Matrix2D_Rotation Rotation;	// 定义了一个旋转矩阵
+Matrix2D_Translation Translation; // 定义了一个平移矩阵
 
-Matrix2D_Scaling Scaling; // 定义了一个缩放矩阵，用来给这个三角形模型做变换
-Matrix2D_Rotation Rotation; // 定义了一个旋转矩阵，也是用来给这个三角形模型做变换的
-Matrix2D_Translation Translation; // 定义了一个平移矩阵，也是用来给这个三角形模型做变换的
+// 定义了一些变量,然后就会通过这些变量来创建旋转，平移，缩放矩阵的，来给这个三角形模型进行矩阵变换的
+double r;	// 这个三角形模型绕着世界坐标系旋转的角度
+double sx;	// 这个三角形模型向x方向缩放多少倍
+double sy;	// 这个三角形模型向y方向缩放多少倍
+double tx;	// 这个三角形模型向x方向平移多少距离
+double ty;	// 这个三角形模型向y方向平移多少距离
 
-vector<Vector2D> Triangle_Vertex_World; // 定义了一个容器，用来存储这个三角形模型的顶点坐标转化到世界下的坐标，到时候就是用这个三角形模型下的顶点的世界坐标来绘制这个三角形模型的
+vector<Vector2D> Triangle_Vertex_World; // 这个是这个三角形模型下的顶点的世界坐标
+Model2D Triangle; // 定义了一个2D的模型类
 
-void Draw(); // 绘制场景
-void Init(); // 初始化场景
-void Update(); // 更新场景
+void Init();	// 初始化场景，比如说初始化在这个场景中的三角形模型
+void Draw();	// 绘制场景，比如说在这个场景中绘制一个三角形模型，那如何绘制呢？，这是通过这个模型的顶点索引来将这个模型给绘制出来的
+void Update();	// 更新场景，用于更新这些在场景中的模型的位置
 
-int main() // 主函数，就是程序的入口
+int main() // 主函数，也就是程序的入口
 {
-	initgraph(800, 600, EX_SHOWCONSOLE); // 初始化这个图形窗口
-	setbkcolor(BLACK); // 设置图形窗口的背景颜色
-	cleardevice(); // 清楚绘图缓冲区，并用背景颜色填充整个窗口
+	/*---------------------初始化窗口---------------------*/
+	initgraph(800, 600, EX_SHOWCONSOLE); // 创建窗口
+	setbkcolor(BLACK); // 设置窗口的背景颜色
+	cleardevice(); // 清除窗口绘图缓冲区，并用背景颜色来填充整个窗口
 
 	Init(); // 初始化场景
 	BeginBatchDraw(); // 开启双缓冲绘图
+
 	while (Running)
 	{
-		while (peekmessage(&msg, EX_KEY))
+		while (peekmessage(&msg, EX_KEY)) // 获取消息，并把获取到的消息存储到消息结构体中
 		{
-			switch (msg.message) // 判断消息类型
+			switch (msg.message) // 判断获取到的消息的类型
 			{
-				case WM_KEYDOWN:
-					switch (msg.vkcode) // 获取用户按下的什么键
+				case WM_KEYDOWN: // 键盘按下消息
+					switch (msg.vkcode) // 通过vkcode成员来获取用户按下了什么键
 					{
+						// 通过键盘上的WSAD来控制这个模型的移动
 						case 'W':
 							ty -= 5;
 							break;
@@ -63,17 +69,31 @@ int main() // 主函数，就是程序的入口
 						case 'D':
 							tx += 5;
 							break;
+
+						// 然后再通过键盘上的RL来控制这个模型绕着世界坐标系下的旋转
 						case 'L':
 							r += 0.1;
 							break;
 						case 'R':
 							r -= 0.1;
 							break;
+
+						// 然后再通过这个键盘上的左右键，来控制这个模型的旋转
 						case VK_LEFT:
 							R += 0.1;
 							break;
 						case VK_RIGHT:
 							R -= 0.1;
+							break;
+
+						// 然后再通过键盘上的上键和下键来控制这个模型的缩放
+						case VK_UP:
+							sx += 1;
+							sy += 1;
+							break;
+						case VK_DOWN:
+							sx -= 1;
+							sy -= 1;
 							break;
 						default:
 							break;
@@ -83,33 +103,34 @@ int main() // 主函数，就是程序的入口
 					break;
 			}
 		}
-		cleardevice(); // 清除绘图缓冲区
+		cleardevice(); // 清除窗口绘图缓冲区，并用背景颜色来填充整个窗口
 		Update(); // 更新场景
 		Draw(); // 绘制场景
 		FlushBatchDraw(); // 刷新绘图缓冲区
 	}
+
 	EndBatchDraw(); // 关闭双缓冲绘图
-	return 0; // 程序的返回值，如果返回0，代表程序正常退出
+	return 0; // 程序的返回值，如果返回0，代表程序正常退出，否则就是程序异常退出
 }
 
-void Init()
+void Init() // 初始化场景
 {
-	/*-----------------初始化这个场景中三角形模型-----------------*/
-	// 初始化这个三角形模型的顶点
+	/*-------------------初始化三角形模型-------------------*/
+	// 初始化这个三角形模型的顶点坐标
 	vector<Vector2D> Triangle_Vertex;
 	Triangle_Vertex.push_back(Vector2D(0, 50, 1));
 	Triangle_Vertex.push_back(Vector2D(0, 0, 1));
 	Triangle_Vertex.push_back(Vector2D(50, 0, 1));
 	Triangle.Set_Vertex(Triangle_Vertex);
 
-	// 初始化这个三角形模型的顶点索引
+	// 初始化这个三角形模型的顶点索引，那顶点索引有什么用呢，首先我们可以通过这个模型的顶点索引来描述这个模型的形状，其次就是我们可以通过这个模型的顶点索引来将这个模型给绘制出来
 	vector<Vector2D> Triangle_Vertex_Index;
 	Triangle_Vertex_Index.push_back(Vector2D(0, 1, 1));
 	Triangle_Vertex_Index.push_back(Vector2D(0, 2, 1));
 	Triangle_Vertex_Index.push_back(Vector2D(1, 2, 1));
 	Triangle.Set_Vertex_Index(Triangle_Vertex_Index);
 
-	// 设置这个三角形模型在世界坐标系中的坐标和这个三角形模型的旋转角度
+	// 初始化这个三角形模型在世界坐标系中的位置,也就是这个模型下的模型坐标系中的原点在世界坐标系中的坐标和这个模型的旋转角度
 	X = 100;
 	Y = 100;
 	R = 0;
@@ -132,40 +153,57 @@ void Init()
 	Rotation.SetR(r);
 }
 
-void Update()
+void Update() // 更新场景，用于这个的模型坐标变换和将这个模型的下的坐标系的顶点坐标转化到世界坐标,然后绘制这个模型的时候是通过这个模型下的顶点的世界坐标来将这个模型给绘制出来的
 {
-	Rotation.SetR(r);
+	/*---------------------更新这些变换矩阵---------------------*/
+	Scaling.SetS(sx, sy);
 	Translation.SetT(tx, ty);
+	Rotation.SetR(r);
 
-	X = Triangle.GetModelX();
-	Y = Triangle.GetModelY();
-	Vector2D v(X, Y, 1);
+	/*---------------------通过这些矩阵来给这个三角形模型做变换---------------------*/
+	// 通过获取到这个模型在世界坐标系中x坐标和y坐标，并用获取到的坐标来创建一个临时的向量
+	Vector2D v(Triangle.GetModelX(), Triangle.GetModelY(), 1);
+
+	// 给这个通过模型坐标来创建向量的进行矩阵的变换，来实现这个三角形模型绕世界坐标系中的旋转和这个三角形模型的平移和三角形模型的缩放
 	v = (Translation * Rotation) * v;
+
+	// 然后再通过这个缩放矩阵来实现这个模型的缩放
+	// 首先先获取这个模型的顶点坐标
+	vector<Vector2D> Model_Vertex = Triangle.Get_Vertex();
+
+	//然后就通过获取到的模型的顶点坐标来进行缩放的变换
+	for (int i = 0; i < Model_Vertex.size(); i++)
+	{
+		Model_Vertex[i] = Scaling * Model_Vertex[i];
+	}
+	Triangle.Set_Vertex(Model_Vertex);
+
+	// 更新这个模型的x坐标和y坐标和这个模型的旋转角度
 	Triangle.SetModelX(v[0]);
 	Triangle.SetModelY(v[1]);
 	Triangle.SetModelR(R);
-	Triangle_Vertex_World = Matrix2D::Model_To_World(Triangle);
-	cout << "x: " << v[0] << "\t" << "y: " << v[1] << endl;
 
+	Triangle_Vertex_World = Matrix2D::Model_To_World(Triangle); // 用于将这个三角形模型下的顶点坐标转化到世界坐标
+
+	// 初始化这些变量，因为是通过这些变量来创建的矩阵，如果不初始化就会导致叠加上一次变换
+	r = 0;
+	sx = 1;
+	sy = 1;
 	tx = 0;
 	ty = 0;
-	r = 0;
 }
 
-void Draw()
+void Draw() // 绘制场景
 {
 	setlinecolor(WHITE); // 设置线条颜色
 
-	/*-----------------绘制这个场景中三角形模型-----------------*/
-	// 获取这个三角形模型的顶点和这个三角形模型的顶点索引
-	// vector<Vector2D> Triangle_Vertex = Triangle.Get_Vertex();
-	vector<Vector2D> Triangle_Vertex_Index = Triangle.Get_Vectex_Index();
-
-	// 然后再根据这个三角形模型的顶点索引把这个三角形给回绘制出来，那怎么绘制呢？，其实很简单，首先，先获取当前索引的第0个，和第1个，因为每一个的索引就是一条线，也就是说，先获取这条线的起始坐标，和这条线的结束坐标，然后在通过这个索引去访问顶点坐标里的的x坐标和y坐标，然后就可以将这条线给绘制出来，循环整个的顶点索引，就可以把这个2D的模型给绘制出来
-	for (int i = 0; i < Triangle_Vertex_Index.size(); i++)
+	/*------------------------------------------绘制这个三角形模型------------------------------------------*/
+	vector<Vector2D> v = Triangle.Get_Vectex_Index(); // 获取这个这个三角形模型顶点索引，通过这个三角形模型的顶点索引来将这个模型给绘制出来
+	for (int i = 0; i < v.size(); i++)
 	{
-		int index = Triangle_Vertex_Index[i][0];
-		int index1 = Triangle_Vertex_Index[i][1];
-		line(Triangle_Vertex_World[index][0], Triangle_Vertex_World[index][1], Triangle_Vertex_World[index1][0], Triangle_Vertex_World[index1][1]);
+		// 那是如何通过这些三角形模型的顶点索引来将这个模型给绘制出来的？因为每一个的三角形模型的顶点的索引就是一条线，并且通过当前索引的第一个索引就是这条线的起始索引，然后第二个索引就是这条线的结束索引，然后就是通过这个索引去访问这个模型的顶点的世界坐标来将这个模型给绘制出来
+		int index1 = v[i][0]; // 获取当前索引的第一个元素也就是这条线的起始索引
+		int index2 = v[i][1]; // 获取当前索引的第二个元素也就是这条线的结束索引
+		line(Triangle_Vertex_World[index1][0], Triangle_Vertex_World[index1][1], Triangle_Vertex_World[index2][0], Triangle_Vertex_World[index2][1]); // 通过这条线的起始索引和结束索引，来去访问这个三角形模型顶点的世界坐标，来将这个模型给绘制出来
 	}
 }
